7b9d8ea9dfbdf4dae9ed8ebd2e9f820c0049093c
nmatzke <matzke@berkeley.edu>
2020-01-13 16:35:00 +1300

    added tests for areas_list_to_states_list

--- test/runtests.jl
+++ test/runtests.jl
@@ -24,5 +24,28 @@ end
 	@test numstates_from_numareas(10,10,false) == 1023
 	@test numstates_from_numareas(10,10,true) == 1024
 	@test numstates_from_numareas(20,20,true) == 1048576
+	
+	# Set up list of areas
+	area_nums = collect(1:3)
+	
+	tmpstr = "Array{Any,1}[[1], [2], [3]]"
+	states_list_answer = eval(Meta.parse(tmpstr))
+	states_list = areas_list_to_states_list(area_nums, 1, false)
+	@test states_list == states_list_answer
+
+	tmpstr = "Array{Any,1}[[], [1], [2], [3]]"
+	states_list_answer = eval(Meta.parse(tmpstr))
+	states_list = areas_list_to_states_list(area_nums, 1, true)
+	@test states_list == states_list_answer
+
+	tmpstr = "Array{Any,1}[[1], [2], [3], [1, 2], [1, 3], [2, 3], [1, 2, 3]]"
+	states_list_answer = eval(Meta.parse(tmpstr))
+	states_list = areas_list_to_states_list(area_nums, 3, false)
+	@test states_list == states_list_answer
+	
+	tmpstr = "Array{Any,1}[[], [1], [2], [3], [1, 2], [1, 3], [2, 3], [1, 2, 3]]"
+	states_list_answer = eval(Meta.parse(tmpstr))
+	states_list = areas_list_to_states_list(area_nums, 3, true)
+	@test states_list == states_list_answer
 end
 
