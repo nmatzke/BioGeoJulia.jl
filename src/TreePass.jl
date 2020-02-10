be8829e6bad2c156922a70312db72d8448102b6e
nmatzke <matzke@berkeley.edu>
2020-02-10 17:42:45 +1300

    Update TreePass.jl

--- src/TreePass.jl
+++ src/TreePass.jl
@@ -1383,6 +1383,9 @@ function nodeOp_ClaSSE_v5(current_nodeIndex, res; p_Ds_v5)
 		sum_likes_at_node = sum(nodeData_at_top)
 		#sum_likes_at_node = 1.0
 		res.likes_at_each_nodeIndex_branchTop[current_nodeIndex] = (nodeData_at_top .+ 0.0)
+		print("\n\ncurrent_nodeIndex:")
+		print(current_nodeIndex)
+		print("\n")
 		res.normlikes_at_each_nodeIndex_branchTop[current_nodeIndex] = (nodeData_at_top .+ 0.0)
 		res.normlikes_at_each_nodeIndex_branchTop[current_nodeIndex] = (res.normlikes_at_each_nodeIndex_branchTop[current_nodeIndex] ./ sum_likes_at_node)
 		res.likes_at_each_nodeIndex_branchTop[current_nodeIndex]
