0e772885bf4a5268c4cb7cb1717f9d830f3237ad
nmatzke <matzke@berkeley.edu>
2020-02-17 21:04:38 +1300

    Update StateSpace.jl

--- src/StateSpace.jl
+++ src/StateSpace.jl
@@ -434,7 +434,6 @@ Qarray_ivals = Qmat.Qarray_ivals
 Qarray_jvals = Qmat.Qarray_jvals
 Qij_vals = Qmat.Qij_vals
 event_type_vals = Qmat.event_type_vals
-
 hcat(Qarray_ivals, Qarray_jvals, Qij_vals, event_type_vals)
 
 
