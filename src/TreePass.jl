7f09944f47aae6b2f6827673b262b5207b2852eb
nmatzke <matzke@berkeley.edu>
2020-07-07 12:18:37 +1200

    Update TreePass.jl

--- src/TreePass.jl
+++ src/TreePass.jl
@@ -1252,6 +1252,8 @@ nodeOp_Cmat = (tmpDs; tmp1, tmp2, p_Ds_v5) -> begin
 	Carray_jvals = p.p_indices.Carray_jvals
 	Carray_kvals = p.p_indices.Carray_kvals
 	
+	display("Running nodeOp_Cmat:\n\n")
+	
 	# Calculate likelihoods of states just before speciation
   @inbounds for i in 1:n
 		Ci_sub_i = p.p_TFs.Ci_sub_i[i]
@@ -1283,6 +1285,13 @@ nodeOp_Cmat = (tmpDs; tmp1, tmp2, p_Ds_v5) -> begin
 		
 		# Parameter values for these events with nonzero rates
 		tmpDs[i] = sum(Cijk_vals[Ci_sub_i] .* (tmp1[Cj_sub_i] .* tmp2[Ck_sub_i]))
+		display(tmpDs[i])
+		dislay(Cijk_vals)
+		display(Ci_sub_i)
+		display(Cj_sub_i)
+		display(Ck_sub_i)
+		display(tmp1)
+		display(tmp2)
   end
   return(tmpDs)
 end
