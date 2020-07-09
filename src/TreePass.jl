a0729a14c5e0c37db161f0076e3183ea772aa9b5
nmatzke <matzke@berkeley.edu>
2020-07-09 16:58:24 +1200

    res.logsum_likes_at_nodes[current_nodeIndex] = log(sum_likes_at_node) + 0.0 + node_lnL

--- src/TreePass.jl
+++ src/TreePass.jl
@@ -1468,15 +1468,15 @@ function nodeOp_ClaSSE_v5(current_nodeIndex, res; p_Ds_v5)
 		
 		sum_likes_at_node = sum(nodeData_at_top)
 		#sum_likes_at_node = 1.0
-		res.likes_at_each_nodeIndex_branchTop[current_nodeIndex] = (nodeData_at_top .+ 0.0) .* exp(node_lnL)
+		res.likes_at_each_nodeIndex_branchTop[current_nodeIndex] = (nodeData_at_top .+ 0.0)
 		res.likes_at_each_nodeIndex_branchTop[current_nodeIndex]
 		print("\n\ncurrent_nodeIndex:")
 		print(current_nodeIndex)
 		print("\n")
-		res.normlikes_at_each_nodeIndex_branchTop[current_nodeIndex] = ((nodeData_at_top .+ 0.0) ./ sum_likes_at_node) .* exp(node_lnL)
+		res.normlikes_at_each_nodeIndex_branchTop[current_nodeIndex] = ((nodeData_at_top .+ 0.0) ./ sum_likes_at_node)
 		
-		res.sum_likes_at_nodes[current_nodeIndex] = (sum_likes_at_node + 0.0) * exp(node_lnL)
-		res.logsum_likes_at_nodes[current_nodeIndex] = log(sum_likes_at_node) + 0.0
+		res.sum_likes_at_nodes[current_nodeIndex] = sum_likes_at_node + 0.0
+		res.logsum_likes_at_nodes[current_nodeIndex] = log(sum_likes_at_node) + 0.0 + node_lnL
 # 		print("\nnodeData_at_top:\n")
 # 		print(nodeData_at_top)
 # 
