231671d3385a321bb19b1194baaa9515004879d4
wallismiyuki <wbla447@aucklanduni.ac.nz>
2020-07-09 13:33:30 +1200

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -56,6 +56,133 @@ end
 	states_list_answer = eval(Meta.parse(tmpstr))
 	states_list = areas_list_to_states_list(area_nums, 3, true)
 	@test states_list == states_list_answer
+
+	# still need to be done:
+	"""
+	@test default_Cparams() 
+	Cparams == CparamsStructure(1.0, 1.0, 1.0, 0.0)
+	# default_Cparams
+
+	note I'm getting 'Cparams not defined' which makes no sense at that
+	literally in the definition of the function? the output of default_cparams() is
+	still CparamsStructure(1.0, 1.0, 1.0, 0.0) (however when this is tested, still comes back false
+	when compared to CparamsStructure(1.0, 1.0, 1.0, 0.0), so not sure what's going on there)
+	
+	# sumy
+	# sums
+	# sumv
+	# sumj
+	unsure what these do
+	"""
+	# get_default_inputs prt not defined?
+	# run_model
+	
+	tmpstr = "(mu_vals = [0.1, 0.1], Qij_vals = [0.01, 0.001], Cijk_vals = [0.222222, 0.222222])"
+	p_Es_v5 = setup_MuSSE(2; birthRate=0.222222, deathRate=0.1, q01=0.01, q10=0.001)
+	p_Es_v5_tmp =  eval(Meta.parse(tmpstr))
+	@test p_Es_v5.params == p_Es_v5_tmp
+	
+	tmpstr = "(mu_vals = [0.1, 0.1], Qij_vals = [0.01, 0.001], Cijk_vals = [0.333333, 0.333333])"
+	p_Es_v5 = setup_MuSSE(2; birthRate=0.333333, deathRate=0.1, q01=0.01, q10=0.001)
+	p_Es_v5_tmp =  eval(Meta.parse(tmpstr))
+	@test p_Es_v5.params == p_Es_v5_tmp
+
+	tmpstr = "(mu_vals = [0.2, 0.2], Qij_vals = [0.01, 0.001], Cijk_vals = [0.222222, 0.222222])"
+	p_Es_v5 = setup_MuSSE(2; birthRate=0.222222, deathRate=0.2, q01=0.01, q10=0.001)
+	p_Es_v5_tmp =  eval(Meta.parse(tmpstr))
+	@test p_Es_v5.params == p_Es_v5_tmp
+
+	tmpstr = "(mu_vals = [0.1, 0.1], Qij_vals = [0.2, 0.001], Cijk_vals = [0.222222, 0.222222])"
+	p_Es_v5 = setup_MuSSE(2; birthRate=0.222222, deathRate=0.1, q01=0.2, q10=0.001)
+	p_Es_v5_tmp =  eval(Meta.parse(tmpstr))
+	@test p_Es_v5.params == p_Es_v5_tmp
+
+	tmpstr = "(mu_vals = [0.1, 0.1], Qij_vals = [0.01, 0.2], Cijk_vals = [0.222222, 0.222222])"
+	p_Es_v5 = setup_MuSSE(2; birthRate=0.222222, deathRate=0.1, q01=0.01, q10=0.2)
+	p_Es_v5_tmp =  eval(Meta.parse(tmpstr))
+	@test p_Es_v5.params == p_Es_v5_tmp
+
+	# setup_DEC_DEmat
+	numareas = 3
+	areas_list = collect(1:numareas)
+	states_list = areas_list_to_states_list(areas_list, 3, true)
+	numstates = length(states_list)
+	amat = reshape(collect(1:(numareas^2)), (numareas,numareas))
+	dmat = reshape(collect(1:(numareas^2)), (numareas,numareas)) ./ 100
+	elist = repeat([0.123], numstates)
+	allowed_event_types=["d","e"]
+
+	Qmat = setup_DEC_DEmat(areas_list, states_list, dmat, elist, amat)
+	@test Qmat.Qarray_jvals[4] == 7
+
+	states_list = areas_list_to_states_list(areas_list, 3, false)
+	Qmat = setup_DEC_DEmat(areas_list, states_list, dmat, elist, amat; allowed_event_types=["d","e"])
+	@test Qmat.Qarray_jvals[4] == 6
+
+	states_list = areas_list_to_states_list(areas_list, 3, false)
+	Qmat = setup_DEC_DEmat(areas_list, states_list, dmat, elist, amat; allowed_event_types=["a"])
+	@test Qmat.Qarray_jvals[4] == 1
+
+	# update_Qij_vals
+	Qmat1_df = hcat(Qarray_ivals, Qarray_jvals, Qij_vals, event_type_vals)
+	dmat = reshape(repeat([0.5], numareas^2), (numareas,numareas))
+	"""
+	Qmat2 = update_Qij_vals(Qmat, areas_list, states_list, dmat, elist, amat )
+	ERROR: MethodError: Cannot `convert` an object of type Array{Int64,2} to an object of type Float64
+	I SEE NO CONVERT-LIKE MOVES WITHIN THIS FUNCTION, I AM UNSURE WHERE IT IS TRYING TO CONVERT ANYTHING
+	"""
+	# relative_probabilities_of_subsets
+	# relative_probabilities_of_vicariants()
+	# discrete_maxent_distrib_of_smaller_daughter_ranges()
+	"""
+	relative_probabilities_of_subsets()
+	ERROR: ArgumentError: MathProgBase solvers like `solve!(problem, SCSSolver())` are no longer supported. Use instead e.g. `solve!(problem, SCS.Optimizer)`.
+	
+	Same issue for 
+	relative_probabilities_of_vicariants()
+	discrete_maxent_distrib_of_smaller_daughter_ranges()
+	
+	"""
+
+	# array_in_array
+	
+
+	# is_event_vicariance
+	ancstate = [1, 2, 3,4];
+	lstate = [1, 2];
+	rstate = [4];
+	@test is_event_vicariance(ancstate, lstate, rstate) == false
+
+	ancstate = [1, 2, 3,4];
+	lstate = [1, 2];
+	rstate = [2, 4];
+	@test is_event_vicariance(ancstate, lstate, rstate) == false
+
+	ancstate = [1, 2, 3,4];
+	lstate = [1, 2];
+	rstate = [3, 4];
+	@test is_event_vicariance(ancstate, lstate, rstate) == true
+
+	# setup_DEC_Cmat
+	"""
+	areas_list = [1,2,3]
+	states_list = areas_list_to_states_list(areas_list, 3, true)
+	Cparams=(y=1.0,s=1.0,v=1.0,j=0.0)
+	max_numareas = length(areas_list)
+	maxent_constraint_01 = 0.0
+	maxent01symp = relative_probabilities_of_subsets(max_numareas, maxent_constraint_01)
+	maxent01sub = relative_probabilities_of_subsets(max_numareas, maxent_constraint_01)
+	maxent01jump = relative_probabilities_of_subsets(max_numareas, maxent_constraint_01)
+	maxent_constraint_01 = 0.5
+	maxent01vic = relative_probabilities_of_vicariants(max_numareas, maxent_constraint_01)
+	maxent01 = (maxent01symp=maxent01symp, maxent01sub=maxent01sub, maxent01vic=maxent01vic, maxent01jump=maxent01jump)
+	predeclare_array_length=10000000
+	Carray = setup_DEC_Cmat(areas_list, states_list, Cparams)
+
+	Cannot be run because relative_probabilities_of_subsets cannot be run!
+	"""
+
+	# update_Cijk_vals
 end
 
 @testset "TrUtils" begin
@@ -181,5 +308,11 @@ end
 
 
 @testset "SSEs" begin
+	# parameterized_ClaSSE
+	# parameterized_ClaSSE_Es
+	# parameterized_ClaSSE_Ds
+	# parameterized_ClaSSE_v5
+	# parameterized_ClaSSE_Es_v5
+	# parameterized_ClaSSE_Ds_v5
 
 end
