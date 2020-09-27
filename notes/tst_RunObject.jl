e3ca6ac1423e3df9d3b6b9b7e477001dda64a271
nmatzke <matzke@berkeley.edu>
2020-09-27 21:40:48 +1300

    Update tst_RunObject.jl

--- notes/tst_RunObject.jl
+++ notes/tst_RunObject.jl
@@ -22,4 +22,17 @@ module Tst_RunObject
 
 	RunObject.say_hello4()
 	
+	
+	# construct the sub-objects, then the full one
+	lgdata_fn = "/GitHub/BioGeoJulia.jl/ex/homs1/homs_geog.txt"
+	geog_df = Parsers.getranges_from_LagrangePHYLIP(lgdata_fn)
+
+	tr_bkup = readTopology("(((human:0.5):0.5,(chimp:0.5):0.5):1.0,(gorilla:1.5):0.5);")
+	trfn = "/GitHub/BioGeoJulia.jl/ex/homs1/homs_w_breakpoint.newick"
+	tr = readTopology(trfn)
+	trdf = prt(tr)
+
+	ro = RunObject.construct_RunObject(trfn, lgdata_fn)
+
+	
 end # End of module
